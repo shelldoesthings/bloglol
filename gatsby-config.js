1f5784afc5682f77865d72fe1c26aad3fb1f5214
shelldoesthings <joshuatobin43@gmail.com>
2021-03-27 23:05:20 +1100

    added the gatsby source filesystem!

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`
+        path: `${__dirname}/src/pages`
+      },
+    },    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`
+        path: `${__dirname}/src/posts`
+      },
+    },    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`
+        path: `${__dirname}/src/images`
+      },
+    },
+  ],
+}
