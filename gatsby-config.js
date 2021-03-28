0a5cad9b185202ac2f37b8da05c69c5f9b7fd681
shelldoesthings <joshuatobin43@gmail.com>
2021-03-28 16:18:03 +1100

    hotfixing

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,19 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`
-        path: `${__dirname}/src/pages`
+        path: `${__dirname}/src/pages`,
       },
     },    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`
-        path: `${__dirname}/src/posts`
+        path: `${__dirname}/src/posts`,
       },
     },    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-plugin-sharp`,
@@ -28,14 +28,8 @@ module.exports = {
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`
-            _options: {
+            options: {
               maxWidth: 1200,
-            },
-            get options() {
-              return this._options;
-            },
-            set options(value) {
-              this._options = value;
             },
           },
         ]
